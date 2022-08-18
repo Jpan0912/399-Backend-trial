2e7700997a384fa0be841f267b924664b159f689
Jpan0912 <74218391+Jpan0912@users.noreply.github.com>
2022-08-18 23:07:30 +1200

    annotating

--- anticheat/views.py
+++ anticheat/views.py
@@ -5,12 +5,24 @@ from .models import Person
 from .serializers import PersonSerializer
 
 
+
+#Example code
 def people_list(request):
     people = Person.objects.all()
     serializer = PersonSerializer(people, many=True)
     return JsonResponse(serializer.data, safe=False)
 
-
+#Webscraping code
 @api_view(['GET'])
 def GetUrls(request):
+    """
+
+
+    :param request:  - which would be your filename
+
+    :code - followed by your webscraping code
+    :return: - can workout the response variable but potentially a Json Object containing information such as what site and the respective URL.
+    """
+
+
     return HttpResponse('Example')
